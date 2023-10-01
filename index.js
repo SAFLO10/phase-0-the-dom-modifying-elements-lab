69ab1f4a88f99cf42a36a106de4a6db3dd8b4a86
Sebastian <seb154@live.com>
2023-10-01 17:21:45 -0400

    modifies index.js and Adds package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+mainNode.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = "Seba";
+newHeader.textContent = "${Seba} is the champion";
+console.log(newHeader.textContent);
+document.body.appendChild(newHeader);
